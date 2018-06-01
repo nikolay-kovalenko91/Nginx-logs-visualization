45b350e45341cedfbe0af923c41a83188a16ce39
Nikolay Kovalenko <nickolaykovalenko@improvado.io>
2018-06-01 22:04:57 +0700

    Migration to a config file

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 
 # log_format ui_short '$remote_addr $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER"
 #                     '$request_time';
 
-config = {
+default_config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log"
@@ -47,10 +48,19 @@ class LogAnalyzer:
         self._write_report(source)
 
 
+def _get_config(path):
+    try:
+        with open(path) as config_file:
+            return json.load(config_file)
+    except (IOError, OSError):
+        print("An error opening / processing config file occurred")
+
 def main():
-    log_path = os.environ.get('NGINX_LOG_PATH',
-                              './nginx-access-ui.log-20170630.gz')
-    LogAnalyzer().main(log_path)
+    config_path = os.environ.get('config')
+    config = default_config
+    if config_path:
+        config = _get_config(config_path)
+    #LogAnalyzer().main(log_path=config[])
 
 
 if __name__ == '__main__':
